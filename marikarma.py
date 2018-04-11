797affb7f0d12bb437464efdc80ab5568ace89d8
buddhasimon <38208046+buddhasimon@users.noreply.github.com>
2018-04-11 15:57:46 +0300

    polished

--- marikarma.py
+++ marikarma.py
@@ -15,17 +15,17 @@ class BotHandler:
         	result_json = resp.json()['result']
         	return result_json
 
-    	def send_message(self, chat_id, text):
-        	params = {'chat_id': chat_id, 'text': text}
-        	method = 'sendMessage'
-        	resp = requests.post(self.api_url + method, params)
-        	return resp
+	def send_message(self, chat_id, text):
+		params = {'chat_id': chat_id, 'text': text}
+		method = 'sendMessage'
+		resp = requests.post(self.api_url + method, params)
+		return resp
+	
+	def get_last_update(self):
+		get_result = self.get_updates()
 
-    	def get_last_update(self):
-        	get_result = self.get_updates()
-
-        	if len(get_result) > 0:
+		if len(get_result) > 0:
 			last_update = get_result[-1]
-        	else:
+		else:
 			last_update = get_result[len(get_result)]
 		return last_update
